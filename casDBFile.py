5ec9f0ee29fa0cd2c0c5fb7d2f515d748e93168d
ristllin <32160010+ristllin@users.noreply.github.com>
2019-01-18 22:34:35 +0200

    Add files via upload

---
+++ casDBFile.py
@@ -0,0 +1,38 @@
+casDB = ["NGG","NGCG","NGAG","NGAN","NNGRRT","NNNNGATT","NNAGAAW","NAAAAC","NNNVRYM","TTTN","NNGTGA","YTN","BGT","GGC","YGA","NNNRRT"]
+CASletter = {
+                "N":["A","C","T","G"],
+                "A":["A"],
+                "C":["C"],
+                "T":["T"],
+                "G":["G"],
+                "U":["U"],
+                "W": ["A","T"],
+                "S": ["C","G"],
+                "M": ["A","C"],
+                "K": ["G","T"],
+                "R": ["A","G"],
+                "Y": ["C","T"],
+                "B": ["C","G","T"],
+                "D": ["A","G","T"],
+                "H": ["A","C","T"],
+                "V": ["A","C","G"],
+
+}
+CASnames = {
+    "NGG":"SpCas9",
+    "NGCG":"VRER SpCas9",
+    "NGAG":"EQR SpCas9",
+    "NGAN":"VQR SpCas9",
+    "NNGRRT":"SaCas9",
+    "NNNRRT":"KKH SaCas9",
+    "NNNNGATT":"NmCas9",
+    "NNAGAAW":"StCas9",
+    "NAAAAC":"TdCas9",
+    "NNNVRYM":"CjCas9",
+    "NNGTGA":"SpCas9 (From Strep)",
+    "TTTN":"LbCpf1 & AsCpf1",
+    "YTN":"FnCpf1",
+    "BGT":"xCas9",
+    "GGC":"xCas9",
+    "YGA":"xCas9",
+}
